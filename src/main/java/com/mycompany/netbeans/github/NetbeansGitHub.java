a7be1151a02d039f7ace6d3c5f4fdd897f83d029
iago <iagomartinezv@gmail.com>
2023-10-27 22:11:43 +0200

    Primer commit

---
+++ src/main/java/com/mycompany/netbeans/github/NetbeansGitHub.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.netbeans.github;
+
+/**
+ *
+ * @author Usuario
+ */
+public class NetbeansGitHub {
+
+    public static void main(String[] args) {
+    	int x=3;
+	int y=11;
+	int z = suma( x, y);
+	System.out.println(z);
+    }	
+    static int suma(int a, int b) {
+	return a+b;
+    }
+}
